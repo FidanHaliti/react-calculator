8e8764a7c7ea3bd729fda0fbec5588c6eac0ab79
Fidan Haliti <fidan91haliti@gmail.com>
2023-04-23 14:50:54 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+
+
+import './App.css'
+import Calculator from './Components/Calculator'
+import CalculatorUseReducer from './Components/CalculatorUseReducer'
+
+function App() {
+  
+
+  return (
+    <div className="App">
+      <Calculator/>
+      <CalculatorUseReducer/>
+    </div>
+  )
+}
+
+export default App
